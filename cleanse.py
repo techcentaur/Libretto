201fc421ef31c02a766293749d918e3ba6b846de
Ankit Solanki <ankit03june@gmail.com>
2018-05-06 00:06:45 +0530

    ADD: added argparse and wrote main functions

---
+++ cleanse.py
@@ -0,0 +1,64 @@
+import argparse, nltk
+from lyrics import Scraper
+from string import punctuation
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+class Clean:
+	def __init__(self, tokens):
+		self.tokens = tokens
+
+	def apostrophe_normalisation(self):
+		text = " ".join(words)
+		text = re.sub(r"n\'t", " not", text)
+		text = re.sub(r"\'re", " are", text)
+		text = re.sub(r"\'s", " is", text)
+		text = re.sub(r"\'d", " would", text)
+		text = re.sub(r"\'ll", " will", text)
+		text = re.sub(r"\'t", " not", text)
+		text = re.sub(r"\'ve", " have", text)
+		text = re.sub(r"\'m", " am", text)
+
+		w = ntlk.word_tokenize(text)
+		return w
+
+
+	def stopwords_remove(self):		
+		toks = self.tokens
+		stopwords = set(stopwords.words('english'))
+
+		clean_tokens = [x for x in toks if not x in stopwords]
+		return clean_tokens
+
+
+	def punctuation_remove(self):
+		clean_tokens = self.tokens
+		punc_list = list(punctuation)
+
+		for i in clean_tokens:
+			if i in punc_list:
+				clean_tokens.remove(i)
+		
+		return clean_tokens
+
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser(description='Libretto: Analyse songs you like, get results you dont')
+	
+	parser.add_argument('-s', "--song", help="song name", type=str)
+	parser.add_argument('-S', "--singer", help="singer name", type=str)
+	args = parser.parse_args()
+
+	scraper = Scraper(args.song, args.singer)
+	lyrics = scraper.get_lyrics()
+
+	text = lyrics[0]
+	text = text.replace("\n",". ")
+	text = text.replace("\t","")
+	text = text.replace("\r","")
+
+	text_list = text.split(" ")
+
+	clean = Clean(text_list)
+
+	l = clean.punctuation_remove()
