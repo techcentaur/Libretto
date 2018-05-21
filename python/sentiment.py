55fa1612e2126724aac536e69c4a1209240ff763
Ankit Solanki <ankit03june@gmail.com>
2018-05-21 10:37:21 +0530

    NEW: completed the machine learning model and data return function

--- python/sentiment.py
+++ python/sentiment.py
@@ -9,8 +9,9 @@ from nltk.probability import FreqDist, ConditionalFreqDist
 
 
 class Sentiment:
-    def __init__(self, folder_name):
+    def __init__(self, folder_name, execute):
         self.folder_name = folder_name
+        self.execute = execute #['print', 'json']
 
     def feature_dict(self, words):
         return dict([(word, True) for word in words])
@@ -37,3 +38,48 @@ class Sentiment:
             test_features.append((categories_dict[key]['features'])[(categories_dict[key]['cutoff']):]) 
         
         return {'train': train_features, 'test': test_features}
+
+    
+    def machine_model(self, data_dict):
+
+        classifier = NaiveBayesClassifier.train(data_dict['train'])
+
+        reference_set = collections.defaultdict(set)
+        test_set = collections.defaultdict(set)
+
+        for i, (features, key) in enumerate(data_dict['test']):
+            reference_set[key].add(i)
+
+            predict = classifier.classify(features)
+            test_set[predict].add(i)
+
+        if self.execute is 'print':
+            
+            print('Trained on {trainf} instances and tested on {testf} instances ...'.format(trainf=len(data_dict['train']), testf=len(data_dict['test'])))
+            print('Accuracy:', nltk.classify.util.accuracy(classifier, data_dict['test']))
+            
+            print('/------------------- SAD ------------------/')
+            print('Precision:', nltk.metrics.precision(reference_set['sad'], test_set['sad']))
+            print('Recall:', nltk.metrics.recall(reference_set['sad'], test_set['sad']))
+            
+            print('/------------------- CALM -------------------/')
+            print('Precision:', nltk.metrics.precision(reference_set['calm'], test_set['calm']))
+            print('Recall:', nltk.metrics.recall(reference_set['calm'], test_set['calm']))
+            
+            print('/----------------- ENERGETIC -----------------/')
+            print('Precision:', nltk.metrics.precision(reference_set['energetic'], test_set['energetic']))
+            print('Recall:', nltk.metrics.recall(reference_set['energetic'], test_set['energetic']))
+
+            return {}
+        
+        elif self.execute is 'json':
+
+            data = {
+                    'accuracy': nltk.classify.util.accuracy(classifier, data_dict['test']),
+                     'sentiment': {'sad': {'precision': nltk.metrics.precision(reference_set['sad'], test_set['sad']), 'recall': nltk.metrics.recall(reference_set['sad'], test_set['sad'])},
+                        'calm': {'precision': nltk.metrics.precision(reference_set['calm'], test_set['calm']), 'recall': nltk.metrics.recall(reference_set['calm'], test_set['calm'])},
+                        'energetic': {'precision': nltk.metrics.precision(reference_set['energetic'], test_set['energetic']), 'recall': nltk.metrics.recall(reference_set['energetic'], test_set['energetic'])}
+                        }
+                    }
+
+            return data
